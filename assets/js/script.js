a095a299f87950d78dc508edb006329bbcef531d
BrennonSullivan <brennsullivan2@gmail.com>
2023-01-09 19:27:42 -0600

    updated tastedive

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,7 +91,7 @@ var runTMDBSearch = (event => {
 var runTasteDive = (event => {
     searchTerm = $("#search-input").val();
     // cors-anywhere solves cors issue
-    let tasteDriveFetch = "https://cors-anywhere.herokuapp.com/" + "https://tastedive.com/api/similar?q=" + searchTerm +"&verbose=1" + "&k=" + tasteDiveAPI;
+    let tasteDriveFetch = "https://tastedive.com/api/similar?q=" + searchTerm +"&verbose=1" + "&k=" + tasteDiveAPI;
 
     fetch(tasteDriveFetch)
     .then((response) => {
